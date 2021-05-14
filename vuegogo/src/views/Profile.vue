882e8a1c15d8a0672764f0a45d9f1769e318e665
Andjaradji <andjaradji.rooseno@klikacc.com>
2021-05-14 10:38:22 +0700

    vue bootstrap

---
+++ vuegogo/src/views/Profile.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="profile">
+      <div class="container">
+          <div class="row justify-content-center">
+              <div class="col-lg-6">
+                  <div class="d-flex text-start">
+                    <div class="flex-shrink-0">
+                        <img src="../assets/pp.jpeg" class="user-pic" >
+                    </div>
+                    <div class="flex-grow-1 ms-3">
+                      <h3>{{firstName}} {{lastName}}</h3>  
+                      This is some content from a media component. You can replace this with any content and adjust it as needed. Lorem ipsum bla
+                      bla bla bla. Lorem ipsum lorem ipsum.
+                    </div>
+                </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default {
+  name: 'Profile',
+  data: function () {
+      return {
+          firstName: "Andjaradji ",
+          lastName :"Rooseno"
+      }
+  } ,
+  components: {}
+}
+</script>
+
+<style scoped>
+.user-pic{
+    border-radius: 200%;
+    width: 120px;
+    height: 120px;
+}
+</style>
